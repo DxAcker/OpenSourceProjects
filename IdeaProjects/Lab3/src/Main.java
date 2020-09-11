598e8cb909056b9aa501df227c0b8bed6458c9ea
DxAcker <59123196+DxAcker@users.noreply.github.com>
2020-09-11 19:47:58 +0300

    dots support

--- IdeaProjects/Lab3/src/Main.java
+++ IdeaProjects/Lab3/src/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
     public static String IPtoBin(String ip) {
         String output = "";
-        String[] words = ip.split(" ");
+        String[] words = ip.split("\\.");
         String[] bin_str = new String[words.length];
         for (int i = 0; i < words.length; i++) {
             bin_str[i] = Integer.toBinaryString(Integer.parseInt(words[i]));
-            output = output.concat(bin_str[i] + " ");
+            output = output.concat(bin_str[i] + ".");
         }
 
         return output;
@@ -16,11 +16,11 @@ public class Main {
 
     public static String IPtoDec(String ip) {
         String output = "";
-        String[] words = ip.split(" ");
+        String[] words = ip.split("\\.");
         int[] dec = new int[words.length];
         for (int i = 0; i < words.length; i++) {
             dec[i] = Integer.parseInt(words[i], 2);
-            output = output.concat(dec[i] + " ");
+            output = output.concat(dec[i] + ".");
         }
 
         return output;
@@ -28,14 +28,14 @@ public class Main {
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Input an IP (change every dot('.') into space(' ')): ");
+        System.out.print("Input an IP: ");
         String IPin = in.nextLine();
         System.out.print("Your entered IP type is (1)Dec or (2)Bin?: ");
         short answer = in.nextShort();
         if (answer == 1) {
             String out = IPtoBin(IPin);
             System.out.println("Your binary IP: " + out);
-            if      (out.charAt(0) == '0')                       System.out.println("The class of IP is A.");
+            if      (out.charAt(0) == '0') System.out.println("The class of IP is A.");
             else if (out.charAt(0) == '1' && out.charAt(1) == '0') System.out.println("The class of IP is B.");
             else if (out.charAt(1) == '1' && out.charAt(2) == '0') System.out.println("The class of IP is C.");
             else if (out.charAt(2) == '1' && out.charAt(3) == '0') System.out.println("The class of IP is D.");
@@ -45,7 +45,7 @@ public class Main {
         else {
             String out = IPtoDec(IPin);
             System.out.println("Your decade IP: " + out);
-            if      (IPin.charAt(0) == '0')                       System.out.println("The class of IP is A.");
+            if      (IPin.charAt(0) == '0') System.out.println("The class of IP is A.");
             else if (IPin.charAt(0) == '1' && IPin.charAt(1) == '0') System.out.println("The class of IP is B.");
             else if (IPin.charAt(1) == '1' && IPin.charAt(2) == '0') System.out.println("The class of IP is C.");
             else if (IPin.charAt(2) == '1' && IPin.charAt(3) == '0') System.out.println("The class of IP is D.");
